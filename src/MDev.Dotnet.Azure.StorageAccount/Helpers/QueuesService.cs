618efc44d5437466ce1bb412b27b73c69edc984a
Mathieu Mack <mathieu.mack@viseo.com>
2024-07-18 11:00:55 +0200

    Add suppot for cancelation token for queues service

--- src/MDev.Dotnet.Azure.StorageAccount/Helpers/QueuesService.cs
+++ src/MDev.Dotnet.Azure.StorageAccount/Helpers/QueuesService.cs
@@ -13,12 +13,17 @@ public class QueuesService
 
     public async Task SendMessagesAsync<T>(List<T> messageObjects)
     {
+        await SendMessagesAsync(messageObjects, CancellationToken.None);
+    }
+
+    public async Task SendMessagesAsync<T>(List<T> messageObjects, CancellationToken cancellationToken)
+    {
         if (messageObjects is null)
             return;
 
-        for(int i= 0; i< messageObjects.Count; i++)
+        for (int i = 0; i < messageObjects.Count; i++)
         {
-            await SendMessageAsync(messageObjects[i], i % queueClients.Count);
+            await SendMessageAsync(messageObjects[i], i % queueClients.Count, cancellationToken);
         }
     }
 
@@ -30,7 +35,7 @@ public class QueuesService
     /// <returns></returns>
     public async Task SendMessageAsync<T>(T messageObject)
     {
-        await SendMessageAsync(messageObject, Random.Shared.Next(0, queueClients.Count));
+        await SendMessageAsync(messageObject, CancellationToken.None);
     }
 
     /// <summary>
@@ -39,13 +44,24 @@ public class QueuesService
     /// <typeparam name="T"></typeparam>
     /// <param name="message"></param>
     /// <returns></returns>
-    private async Task SendMessageAsync<T>(T messageObject, int queueIndex)
+    public async Task SendMessageAsync<T>(T messageObject, CancellationToken cancellationToken)
+    {
+        await SendMessageAsync(messageObject, Random.Shared.Next(0, queueClients.Count), cancellationToken);
+    }
+
+    /// <summary>
+    /// Push an object into a Binary message to an available queue
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="message"></param>
+    /// <returns></returns>
+    private async Task SendMessageAsync<T>(T messageObject, int queueIndex, CancellationToken cancellationToken)
     {
         if (messageObject is null)
             return;
 
         // Send async completion request :
         var queueClient = queueClients[queueIndex];
-        await queueClient.SendMessageAsync(new BinaryData(messageObject));
+        await queueClient.SendMessageAsync(new BinaryData(messageObject), cancellationToken: cancellationToken);
     }
 }
