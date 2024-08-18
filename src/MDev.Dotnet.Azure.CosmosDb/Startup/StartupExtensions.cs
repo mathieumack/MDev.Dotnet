673249a3904af79e748434f67c6a934a56c95b43
Mathieu Mack <mathieu.mack@viseo.com>
2024-08-18 22:41:44 +0200

    Add extension to register CosmosDb with IServiceCollection

--- src/MDev.Dotnet.Azure.CosmosDb/Startup/StartupExtensions.cs
+++ src/MDev.Dotnet.Azure.CosmosDb/Startup/StartupExtensions.cs
@@ -19,14 +19,29 @@ public static class StartupExtensions
     public static IHostApplicationBuilder RegisterCosmosDb<T>(this IHostApplicationBuilder builder,
                                                                     TokenCredential credentials) where T : DbContext
     {
+        builder.Services.RegisterCosmosDb<T>(builder.Configuration, credentials);
+        return builder;
+    }
+
+    /// <summary>
+    /// Register cosmosDb database context.
+    /// Create a Scoped <see cref="T"/> object based on <see cref="CosmosDbSettings"/> configuration
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="services"></param>
+    /// <returns></returns>
+    public static IServiceCollection RegisterCosmosDb<T>(this IServiceCollection services,
+                                                                IConfiguration configuration,
+                                                                TokenCredential credentials) where T : DbContext
+    {
         // Register DbContext :
         var cosmosDbSettings = new CosmosDbSettings();
-        builder.Configuration.GetRequiredSection(CosmosDbSettings.SectionName).Bind(cosmosDbSettings, options => options.ErrorOnUnknownConfiguration = true);
-        builder.Services.AddDbContext<T>(options =>
+        configuration.GetRequiredSection(CosmosDbSettings.SectionName).Bind(cosmosDbSettings, options => options.ErrorOnUnknownConfiguration = true);
+        services.AddDbContext<T>(options =>
         {
             options.UseCosmos(cosmosDbSettings.Endpoint, credentials, cosmosDbSettings.DatabaseName);
         });
 
-        return builder;
+        return services;
     }
 }
