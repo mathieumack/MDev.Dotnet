7df92138fd75575528d56d4ce9814f29998e7904
Mathieu Mack <mathieu.mack@viseo.com>
2024-08-19 13:48:10 +0200

    Add more logs for async operation requests

--- src/MDev.Dotnet.Azure.ContainerApps/AsyncOperations/Services/Handlers/AsyncOperationRequestsService.cs
+++ src/MDev.Dotnet.Azure.ContainerApps/AsyncOperations/Services/Handlers/AsyncOperationRequestsService.cs
@@ -2,25 +2,35 @@
 using MDev.Dotnet.AspNetCore.AsyncOperations.Abstracts;
 using MDev.Dotnet.AspNetCore.AsyncOperations.Messages;
 using ServiceCollectionHelpers.AssemblyFinder.Attributes;
+using Microsoft.Extensions.Logging;
 
 namespace MDev.Dotnet.AspNetCore.AsyncOperations.Services.Handlers;
 
 [ServiceRegister(Scope = ServiceLifetime.Scoped)]
 public class AsyncOperationRequestsService
 {
+    private readonly ILogger<AsyncOperationRequestsService> logger;
     private readonly IEnumerable<IAsyncOperationRequestMessageHandler> handlers;
 
-    public AsyncOperationRequestsService(IEnumerable<IAsyncOperationRequestMessageHandler> handlers)
+    public AsyncOperationRequestsService(ILogger<AsyncOperationRequestsService> logger,
+                                            IEnumerable<IAsyncOperationRequestMessageHandler> handlers)
     {
+        this.logger = logger;
         this.handlers = handlers;
     }
 
     internal async Task HandleAsync(AsyncOperationRequestMessage item, CancellationToken cancellationToken)
     {
-        foreach (var handler in handlers)
+        if(!handlers.Any(e => e.HandlerOperationName.Equals(item.OperationName)))
         {
-            if (handler.HandlerOperationName.Equals(item.OperationName))
-                await handler.HandleRequest(item, cancellationToken);
+            logger.LogInformation($"async.operation : No service registered for {item.OperationName}");
+            return;
+        }
+
+        foreach (var handler in handlers.Where(e => e.HandlerOperationName.Equals(item.OperationName)))
+        {
+            logger.LogInformation($"call async.operation.{handler.HandlerOperationName}");
+            await handler.HandleRequest(item, cancellationToken);
         }
     }
 }
