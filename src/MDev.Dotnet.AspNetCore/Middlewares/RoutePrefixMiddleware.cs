917a74bdc568e2b7fd2d65699623dcb388f7bf44
Mathieu Mack <mathieu.mack@viseo.com>
2024-11-07 22:20:21 +0100

    Add custom middleware for route prefixs

---
+++ src/MDev.Dotnet.AspNetCore/Middlewares/RoutePrefixMiddleware.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace MDev.Dotnet.AspNetCore.Middlewares;
+
+internal class RoutePrefixMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly string _routePrefix;
+
+    public RoutePrefixMiddleware(RequestDelegate next, string routePrefix)
+    {
+        _next = next;
+        _routePrefix = routePrefix;
+    }
+    public async Task InvokeAsync(HttpContext context)
+    {
+        context.Request.PathBase = new PathString(_routePrefix);
+        await _next(context);
+    }
+}
