a88e4135d46208e032a964daa011713e85ec7f1a
Mathieu Mack <mathieu.mack@viseo.com>
2025-02-26 12:20:18 +0100

    Remove routes by default

--- src/MDev.Dotnet.Azure.ContainerApps/AsyncOperations/Controllers/v1/DaprHandlerController.cs
+++ src/MDev.Dotnet.Azure.ContainerApps/AsyncOperations/Controllers/v1/DaprHandlerController.cs
@@ -6,10 +6,7 @@ using Asp.Versioning;
 
 namespace MDev.Dotnet.AspNetCore.AsyncOperations.Controllers.v1;
 
-[ApiController]
-[ApiVersion("1")]
 [ApiExplorerSettings(IgnoreApi = true)]
-[Route("api/v{version:apiVersion}/daprhandler")]
 public class DaprHandlerController : ControllerBase
 {
     private readonly AsyncOperationRequestsService asyncOperationRequestsService;
@@ -24,7 +21,6 @@ public class DaprHandlerController : ControllerBase
     /// </summary>
     /// <param name="item"></param>
     /// <returns></returns>
-    [HttpPost("operationrequest")]
     public virtual async Task<IActionResult> OperationRequestAsync([FromBody] AsyncOperationRequestMessage item, CancellationToken cancellationToken)
     {
         await asyncOperationRequestsService.HandleAsync(item, cancellationToken);
