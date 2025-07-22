5e9f623a075c44ee005ce828975f094322451cd5
MACK Mathieu <mathieu.mack@outlook.fr>
2025-07-22 14:54:01 +0200

    Add RequireDaprApiTokenAttribute for API token validation (#42)
    * Add RequireDaprApiTokenAttribute for API token validation
    
    This commit introduces the `RequireDaprApiTokenAttribute` class, which implements the `IAuthorizationFilter` interface. This attribute enforces the presence of a specific API token in request headers for controller actions in an ASP.NET Core application. If the token is missing, empty, or does not match the expected value from the configuration, a 401 Unauthorized response is returned. The class also includes functionality to load the expected token from the application settings.
    
    Resolves #41
    
    * Add capability to remove servers from OpenAPI description file.
    
    #43

--- src/MDev.Dotnet.AspNetCore/Apis/Extensions/OpenApiExtensions.cs
+++ src/MDev.Dotnet.AspNetCore/Apis/Extensions/OpenApiExtensions.cs
@@ -7,16 +7,36 @@ namespace MDev.Dotnet.AspNetCore.Apis.Extensions;
 public static class OpenApiExtensions
 {
     public static IHostApplicationBuilder RegisterOpenApi(this IHostApplicationBuilder builder,
-                                                                bool forceHttpsServers = false)
+                                                                bool forceHttpsServers = false,
+                                                                bool includeServerUrls = true)
     {
         builder.Services.AddOpenApi(options => {
             if (forceHttpsServers)
             {
                 options.AddDocumentTransformer((document, context, cancellationToken) => {
+                    if(document.Servers is null || !document.Servers.Any())
+                    {
+                        return Task.CompletedTask;
+                    }
+
                     foreach (var server in document.Servers)
                     {
                         server.Url = server.Url.Replace("http://", "https://");
                     }
+                    return Task.CompletedTask;
+                });
+            }
+
+            if (!includeServerUrls)
+            {
+                options.AddDocumentTransformer((document, context, cancellationToken) => {
+                    if (document.Servers is null || !document.Servers.Any())
+                    {
+                        return Task.CompletedTask;
+                    }
+
+                    document.Servers.Clear();
+
                     return Task.CompletedTask;
                 });
             }
